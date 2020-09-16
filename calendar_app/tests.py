d2d02d2a420834b0a48d03f30867b1f9d311bd43
adam tucker <a.r.tucker@hotmail.co.uk>
2020-09-16 14:56:25 +0100

    calendar tests and views resctricted

--- calendar_app/tests.py
+++ calendar_app/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase, Client
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth import get_user_model
 
 from .models import Event
@@ -7,22 +7,22 @@ from .models import Event
 class EventTests(TestCase):
     
     def setUp(self):
-        User = get_user_model()
-        user = User.objects.create_user(
-            'test_username', 
-            'test_username@example.com', 
-            'da_password') 
+        self.user = get_user_model().objects.create_user(
+            username = 'test_username', 
+            email = 'test_username@example.com', 
+            password = 'da_password') 
         self.event = Event.objects.create(
-            manage=user,  
+            manage=self.user,  
             avilibility='Booked',
             start_time='2020-09-30 08:00:00+00:00',
             end_time='2020-09-30 17:00:00+00:00',
         )
 
     def test_event_create(self):
-        self.assertEquals(f'{self.event.manage}', 'test_username')
-        self.assertEquals(f'{self.event.start_time}','2020-09-30 08:00:00+00:00')
-        self.assertEquals(f'{self.event.end_time}','2020-09-30 17:00:00+00:00')
+        self.assertEqual(f'{self.event.manage}', 'test_username')
+        self.assertEqual(f'{self.event.avilibility}', 'Booked')
+        self.assertEqual(f'{self.event.start_time}','2020-09-30 08:00:00+00:00')
+        self.assertEqual(f'{self.event.end_time}','2020-09-30 17:00:00+00:00')
 
     def test_calendar_list_view(self):
         response = self.client.get(reverse('calendar_list'))
@@ -35,3 +35,18 @@ class EventTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'Booked')
+
+    def test_event_create_view_logged_in(self):
+        self.client.login(email='test_username@example.com', password='da_password')
+        response = self.client.get(reverse('event_add'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'calendar_create_event_form.html')
+
+    def test_event_create_view_logged_out(self):
+        response = self.client.get(reverse('event_add'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_event_update_view(self):
+        response = self.client.get(self.event.get_absolute_url_edit())
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Update Avilibility')
--- calendar_app/views.py
+++ calendar_app/views.py
@@ -1,20 +1,33 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, FormView, UpdateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.http import Http404
+
 
 from .models import Event
 from .forms import EventForm
 
-class CalendarEventsView(ListView):
+class CalendarEventsView(LoginRequiredMixin, ListView):
     model = Event
     context_object_name = 'event_list'
     template_name = 'calendar_list.html'
+    def get_queryset(self):
+        return Event.objects.filter(manage=self.request.user)
 
-class CalendarEventDetail(DetailView):
+class CalendarEventDetail(LoginRequiredMixin, DetailView):
     model = Event
     context_object_name = 'event'
     template_name = 'calendar_event_detail.html'
+    def get_queryset(self):
+        return Event.objects.filter(manage=self.request.user)
+
+    def get(self, request, *args, **kwargs):
+        try:
+            return super(CalendarEventDetail, self).get(request, *args, **kwargs)
+        except Http404:
+            return redirect(reverse('calendar_list'))
 
 class CalendarEventAdd(LoginRequiredMixin, CreateView):
     model = Event
@@ -26,7 +39,13 @@ class CalendarEventAdd(LoginRequiredMixin, CreateView):
         EventForm.instance.manage = self.request.user
         return super().form_valid(EventForm)
 
-class CalendarEventUpdate(UpdateView):
+class CalendarEventUpdate(LoginRequiredMixin, UpdateView):
     model= Event
     form_class = EventForm
     template_name='calendar_update_event_form.html'
+
+    def get(self, request, *args, **kwargs):
+        try:
+            return super(CalendarEventUpdate, self).get(request, *args, **kwargs)
+        except Http404:
+            return redirect(reverse('calendar_list'))
