6b9e14a5b9f915054696656bba56b197d0350f50
Blindrabit <a.r.tucker@hotmail.co.uk>
2020-11-05 21:16:52 +0000

    shifts api tests added

--- api/tests.py
+++ api/tests.py
@@ -49,3 +49,13 @@ class ShiftAPITest(APITestCase):
         url = f'/api/v1/shifts/{self.shifts.id}/'
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
+        data = { 'manage' : None,  
+                'hospital' : self.hospital.id,
+                'area' : self.area.id,
+                'start_time' : '2020-09-30 08:00:00+00:00',
+                'end_time' :'2020-09-30 18:00:00+00:00',
+        }
+        response = self.client.put(url, data, format='json')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.data['end_time'], '2020-09-30T18:00:00Z')
+
