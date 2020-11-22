d2426b7778d6801a3ecdd2be6c8d73859dbba1ad
adam tucker <a.r.tucker@hotmail.co.uk>
2020-11-22 11:08:20 +0000

    move to desktop

--- api/adapter.py
+++ api/adapter.py
@@ -1,11 +1,14 @@
 from allauth.account.adapter import DefaultAccountAdapter
 
+from users.models import HospitalListModel, IUs
+
 class CustomAccountAdapter(DefaultAccountAdapter):
     def save_user(self, request, user, form, commit=False):
         user = super().save_user(request, user, form, commit)
         data = form.data
         user.dbs_number = data['dbs_number']
         user.save()
-        user.hospitals.set(*data['hospitals'])
+        user.hospitals.add(*data['hospitals'])
         user.area_to_work.set(*data['area_to_work'])
+        user.save()
         return user
